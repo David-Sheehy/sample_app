fcc6befac31b64340bafcd220172cf7ae3a0f7c8
David Sheehy <David.Sheehy.1337@gmail.com>
2014-06-09 02:07:36 -0500

    Added microposts capability and view.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -53,11 +53,20 @@ describe "User pages" do
   
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
 
+    before { visit user_path(user) }
+  
     it { should have_content(user.name) }
     it { should have_title(user.name) }
-  end
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end # end microposts
+  end # end profile page
 
   describe "signup page" do
     before { visit signup_path }
@@ -99,6 +108,7 @@ describe "User pages" do
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
+
   end # end sign up
 
   describe "edit" do
